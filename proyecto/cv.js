7c1eb8f891ce005ead92aa9b7953935aca5cd3a3
Lucilitaaye23 <126517309+Lucilitaaye23@users.noreply.github.com>
2023-02-27 18:29:07 -0300

    proyecto2.0

---
+++ proyecto/cv.js
@@ -0,0 +1,11 @@
+// boton de color oscuro
+document.getElementById('boton_color').addEventListener('click', function () {
+     
+    document.body.style.backgroundColor = 'black';
+    
+    })
+    // boton de color claro
+    document.getElementById('boton_defaul').addEventListener('click', function () {
+    
+        document.body.style.backgroundColor= 'white';
+    })
